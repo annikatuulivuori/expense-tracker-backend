6539b056479b06cdcf0847dc55d1cba70173444f
annika <atuulivuo@gmail.com>
2024-01-27 23:19:05 +0200

    lisää logger

--- src/main/java/com/example/expensetrackerbackend/controller/ExpenseController.java
+++ src/main/java/com/example/expensetrackerbackend/controller/ExpenseController.java
@@ -1,5 +1,7 @@
 package com.example.expensetrackerbackend.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -12,6 +14,7 @@ import dto.Expense;
 @RequestMapping("/api/expenses")
 @CrossOrigin(origins = "http://localhost:3000")
 public class ExpenseController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ExpenseController.class);
     private List<Expense> expenses = new ArrayList<>();
 
     @GetMapping("/")
@@ -21,19 +24,29 @@ public class ExpenseController {
 
     @PostMapping("/addExpense")
     public void addExpense(@RequestBody Expense expense) {
+        LOGGER.info("Received request to addExpense: {} ", expense);
+
         expenses.add(expense);
+
+        LOGGER.info("New expense added successfully.");
     }
 
     @GetMapping("/viewExpenses")
     public List<Expense> viewExpenses() {
+        LOGGER.info("Received request to viewExpenses.");
+
         return expenses;
     }
 
     @DeleteMapping("/deleteExpense/{expenseId}")
     public List<Expense>  deleteExpense(@PathVariable int expenseId) {
+        LOGGER.info("Received request to deleteExpense with id: {}", expenseId);
+
         expenses = expenses.stream()
                 .filter(expense -> expense.getId() != expenseId)
                 .collect(Collectors.toList());
+
+        LOGGER.info("Expense with id {} deleted successfully.", expenseId);
 
         return expenses;
     }
